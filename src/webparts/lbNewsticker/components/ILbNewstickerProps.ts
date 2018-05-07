f1619eefa7953677a9a47fec00fc8cf92cb9cef3
Nicolai Danielsen <NICD@LB.DK>
2018-05-07 10:53:22 +0200

    First commit

---
+++ src/webparts/lbNewsticker/components/ILbNewstickerProps.ts
@@ -0,0 +1,3 @@
+export interface ILbNewstickerProps {
+  description: string;
+}
---
+++ src/webparts/lbNewsticker/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ILbNewstickerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'LbNewstickerWebPartStrings' {
+  const strings: ILbNewstickerWebPartStrings;
+  export = strings;
+}
